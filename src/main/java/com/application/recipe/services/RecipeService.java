1aec1ec399d39979f34d91096974abff1b93f9fc
Felipe Augusto <felipe.93nt@gmail.com>
2021-07-22 21:34:37 -0300

    Service Layer implementation

--- src/main/java/com/application/recipe/services/RecipeService.java
+++ src/main/java/com/application/recipe/services/RecipeService.java
@@ -1,2 +1,27 @@
-package com.application.recipe.services;public class RecipeService {
+package com.application.recipe.services;
+
+import com.application.recipe.models.Recipe;
+import com.application.recipe.repositories.impl.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    @Autowired
+    public RecipeService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public List<String> getRecipesName() {
+        List<Recipe> recipes = this.recipeRepository.getRecipes();
+
+        return recipes.stream()
+                .map(r -> r.getTitle()).collect(Collectors.toList());
+    }
 }
